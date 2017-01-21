b73a97a1b42b4411c43fb94e6a42d67b4ddee790
Swift <swift@wozmac4.local>
2017-01-21 15:14:01 -0200

    implementing the logic of quick look

--- Swift300_Day2_4_QLPreviewControllerNavigation/Swift300_Day2_4_QLPreviewControllerNavigation/ViewController.swift
+++ Swift300_Day2_4_QLPreviewControllerNavigation/Swift300_Day2_4_QLPreviewControllerNavigation/ViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+// Importando o framework
+import QuickLook
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, QLPreviewControllerDataSource, QLPreviewControllerDelegate {
+    
+    // MARK: - Outlets
+    
+    // MARK: - Propriedades
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,53 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Métodos
+    
+    // MARK: - Actions
+    @IBAction func mostrarArquivo(_ sender: AnyObject) {
+        
+        // Criando o nosso quicklook
+        let quicklook = QLPreviewController()
+        
+        quicklook.dataSource = self
+        quicklook.delegate   = self
+        
+        self.navigationController?.pushViewController(quicklook, animated: true)
+        
+    }
+    
+    // MARK: - Métodos de QLPreviewControllerDataSource
+    
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        return 1
+    }
+    
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        
+        // Criando o nome do arquivo
+        let nome = "5_Conselhos"
+        
+        // Criando a referência pro Bundle
+        
+        let raizApp = Bundle.main
+        
+        // Criando o caminho até o nosso arquivo
+        let pathArquivo = raizApp.path(forResource: nome, ofType: ".pdf")
+        
+        // Criando a URL de retorno
+        let urlArquivo = URL(fileURLWithPath: pathArquivo ?? "")
+        
+        // Retornando a URL do arquivo
+        return urlArquivo as QLPreviewItem
+        
+    }
+    
+    // MARK: - Métodos de QLPreviewControllerDelegate
 
+    func previewControllerWillDismiss(_ controller: QLPreviewController) {
+        print("Saiu")
+    }
 
 }
 
