1a5f59c2e0f1a55623a7d02a5d530b496bc8b8c3
Swift <swift@wozmac4.local>
2017-01-21 11:00:12 -0200

    creating project, defining layout, outlets, actions and marking up view controller

---
+++ DesafioGCD/DesafioGCD/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  DesafioGCD
+//
+//  Created by Swift on 21/01/17.
+//  Copyright © 2017 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    // MARK: - Propriedades
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Métodos
+    
+    // MARK: - Actions
+    @IBAction func calcular(_ sender: AnyObject) {
+        
+        
+        
+    }
+    
+}
+
