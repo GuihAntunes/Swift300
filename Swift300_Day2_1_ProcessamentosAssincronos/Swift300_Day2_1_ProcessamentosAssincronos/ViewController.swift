2ed77526c7b2566b298a454c4e1f4fe43e3eb920
Swift <swift@wozmac4.local>
2017-01-21 09:26:32 -0200

    Creating project - Processamentos Assíncronos

---
+++ Swift300_Day2_1_ProcessamentosAssincronos/Swift300_Day2_1_ProcessamentosAssincronos/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Swift300_Day2_1_ProcessamentosAssincronos
+//
+//  Created by Swift on 21/01/17.
+//  Copyright © 2017 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var labelCima: UILabel!
+    
+    @IBOutlet weak var labelBaixo: UILabel!
+    
+    // MARK: - Propriedades
+    
+    // MARK: - View Lyfe Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Métodos
+    
+    // MARK: - Actions
+    
+    @IBAction func iniciar(_ sender: AnyObject) {
+    }
+
+}
+
