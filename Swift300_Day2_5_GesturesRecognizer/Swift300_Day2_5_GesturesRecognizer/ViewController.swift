c4d8eb1b369e5564d47039c703677a9a9f317816
Swift <swift@wozmac4.local>
2017-01-21 17:09:25 -0200

    creating project, groups, marking up and initiating interface

---
+++ Swift300_Day2_5_GesturesRecognizer/Swift300_Day2_5_GesturesRecognizer/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Swift300_Day2_5_GesturesRecognizer
+//
+//  Created by Swift on 21/01/17.
+//  Copyright © 2017 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak var littleView: UIView!
+    
+    // MARK: - Propriedades
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Métodos
+    
+    // MARK: - Actions
+
+
+}
+
