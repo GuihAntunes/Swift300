3298125a2263867672befc82c938699b2bb9d8a6
Swift <swift@wozmac4.local>
2017-01-28 14:45:35 -0200

    Creating project, folders and marking up classes

---
+++ DesafioImagePicker/DesafioImagePicker/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  DesafioImagePicker
+//
+//  Created by Swift on 28/01/17.
+//  Copyright © 2017 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    // MARK: - Properties
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - Actions
+
+
+}
+
