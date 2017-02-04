69d226ba9f11f2498967fbad4ac1a7dc9481b3cb
Swift <swift@WozMac5.local>
2017-02-04 15:10:32 -0200

    implementing message composition logic

--- Swift300_Day4_3_EmailAndMessages/Swift300_Day4_3_EmailAndMessages/ViewController.swift
+++ Swift300_Day4_3_EmailAndMessages/Swift300_Day4_3_EmailAndMessages/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 // Importando o Framework ao projeto
 import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate {
     
     // MARK: - Outlets
     
@@ -28,6 +28,7 @@ class ViewController: UIViewController {
     }
     
     // MARK: - Methods
+    
     
     // MARK: - Actions
     @IBAction func sendEmail(_ sender: Any) {
@@ -72,6 +73,7 @@ class ViewController: UIViewController {
         }else{
             
             // Caso não possa
+            // Refactor this
             let alert = UIAlertController(title: "Alerta!", message: "Não é possível enviar e-mails no momento!", preferredStyle: .alert)
             
             let acaoOK = UIAlertAction(title: "Ok", style: .default, handler: nil)
@@ -86,8 +88,117 @@ class ViewController: UIViewController {
 
     @IBAction func sendMessage(_ sender: Any) {
         
+        // Verificação se é possível fazer o envio da mensagem
+        if MFMessageComposeViewController.canSendText() {
+            
+            // Caso seja possível
+            
+            // Criamos um objeto da classe responsável por escrever mensagens
+            let messageCompose = MFMessageComposeViewController()
+            
+            // Propriedade que define o corpo da mensagem
+            messageCompose.body = "E aí jovem, bão?"
+            
+            // Atribuindo o delegate
+            messageCompose.messageComposeDelegate = self
+            
+        }else{
+            
+            // Caso não seja possível
+            // Refactor this
+            let alert = UIAlertController(title: "Alerta!", message: "Não é possível enviar mensagens no momento!", preferredStyle: .alert)
+            
+            let acaoOK = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            alert.addAction(acaoOK)
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    // MARK: - MFMailComposeViewControllerDelegate Methods
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        
+        switch result {
+        case .cancelled:
+            controller.dismiss(animated: true, completion: nil)
+            // Refactor this
+            let alert = UIAlertController(title: "Cancelado!", message: "Envio cancelado!", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            alert.addAction(okAction)
+
+        case .sent:
+            controller.dismiss(animated: true, completion: nil)
+            // Refactor this
+            let alert = UIAlertController(title: "Enviado!", message: "E-mail enviado!", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            alert.addAction(okAction)
+            
+        case .failed:
+            controller.dismiss(animated: true, completion: nil)
+            // Refactor this
+            let alert = UIAlertController(title: "Falhou!", message: "Falha no envio! Tente novamente", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            alert.addAction(okAction)
+            
+        case .saved:
+            controller.dismiss(animated: true, completion: nil)
+            // Refactor this
+            let alert = UIAlertController(title: "Salvo!", message: "E-mail salvo!", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            alert.addAction(okAction)
+            
+        }
+        
         
         
     }
+    
+    // MARK: - MFMessageComposeViewControllerDelegate Methods
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        
+        switch result {
+        case .cancelled:
+            controller.dismiss(animated: true, completion: nil)
+            // Refactor this
+            let alert = UIAlertController(title: "Cancelado!", message: "Envio cancelado!", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            alert.addAction(okAction)
+            
+        case .failed:
+            controller.dismiss(animated: true, completion: nil)
+            // Refactor this
+            let alert = UIAlertController(title: "Errrrooou!", message: "Falha no envio! Tente novamente mais tarde", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            alert.addAction(okAction)
+            
+        case .sent:
+            controller.dismiss(animated: true, completion: nil)
+            // Refactor this
+            let alert = UIAlertController(title: "Enviada!", message: "Mensagem enviada!", preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            alert.addAction(okAction)
+        }
+        
+    }
+    
+    
 }
 
