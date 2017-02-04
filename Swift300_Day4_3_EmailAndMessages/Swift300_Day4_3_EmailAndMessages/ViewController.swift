f0dcf1f4c7fb0e08568f225d3cb393b03bcb200e
Swift <swift@WozMac5.local>
2017-02-04 14:39:19 -0200

    Adding resources, implementing Mail Composition

--- Swift300_Day4_3_EmailAndMessages/Swift300_Day4_3_EmailAndMessages/ViewController.swift
+++ Swift300_Day4_3_EmailAndMessages/Swift300_Day4_3_EmailAndMessages/ViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+// Importando o Framework ao projeto
+import MessageUI
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    
+    // MARK: - Properties
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,68 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Methods
+    
+    // MARK: - Actions
+    @IBAction func sendEmail(_ sender: Any) {
+        
+        // Verificando se o usuário pode enviar e-mails
+        if (MFMailComposeViewController.canSendMail()) {
+            
+            // Caso possa
+            
+            // Criando o nosso objeto responsável pela criação do nosso e-mail
+            
+            let mailCompose = MFMailComposeViewController()
+            
+            // Fazendo o preenchimento dos campos do e-mail
+            
+            // Método que define o objeto responsável pelo assunto do e-mail
+            
+            mailCompose.setSubject("Interesse na vaga aberta")
+            
+            // Método que define o corpo do e-mail
+            mailCompose.setMessageBody("<strong><i> Boa tarde, gostaria de me candidatar a vaga de diretor da Apple. Soube que recentemente ficou vaga por motivos de força maior e que o atual diretor não está indo muito bem! </i></strong>", isHTML: true)
+            
+            // Método que define os destinatários
+            let arrayDestinatarios = ["hr@apple.com", "workwithus@apple.com", "recruitment@apple.com"]
+            mailCompose.setToRecipients(arrayDestinatarios)
+            
+            // Método que define quem estará em cópia do e-mail
+            let arrayCopia = ["zuck@face.com", "bill@microsoft.com", "tim@bolachaoubiscoito.com"]
+            mailCompose.setCcRecipients(arrayCopia)
+            
+            // Método que define quem estará em cópia oculta
+            let arrayCopiaOculta = ["edwardsnooden@nsa.com", "baidu@baidu.com"]
+            mailCompose.setBccRecipients(arrayCopiaOculta)
+            
+            // Anexando um arquivo (utilizando Mime Type)
+            let imageData = UIImagePNGRepresentation(UIImage(named: "foto.png")!)!
+            mailCompose.addAttachmentData(imageData, mimeType: "image/png", fileName: "ScreenShot")
+            
+            self.present(mailCompose, animated: true, completion: nil)
+            
+            
+        }else{
+            
+            // Caso não possa
+            let alert = UIAlertController(title: "Alerta!", message: "Não é possível enviar e-mails no momento!", preferredStyle: .alert)
+            
+            let acaoOK = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            alert.addAction(acaoOK)
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+        
+    }
 
-
+    @IBAction func sendMessage(_ sender: Any) {
+        
+        
+        
+    }
 }
 
