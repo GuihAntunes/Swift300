366aa4e427162c134a5007ec33af7cbbb9c9f819
Swift <swift@WozMac5.local>
2017-02-04 13:53:07 -0200

    creating project, layout and marking up view controller

---
+++ Swift300_Day4_2_CoreLocationBussola/Swift300_Day4_2_CoreLocationBussola/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Swift300_Day4_2_CoreLocationBussola
+//
+//  Created by Swift on 04/02/17.
+//  Copyright © 2017 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    // MARK: - Properties
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - Actions
+
+
+}
+
