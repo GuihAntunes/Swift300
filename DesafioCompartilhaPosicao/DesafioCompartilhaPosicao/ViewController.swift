5d343c9fedb7312722227bcaf103d039c7bae60d
Swift <swift@WozMac5.local>
2017-02-04 15:38:23 -0200

    Creating map kit and implementing tracking function

--- DesafioCompartilhaPosicao/DesafioCompartilhaPosicao/ViewController.swift
+++ DesafioCompartilhaPosicao/DesafioCompartilhaPosicao/ViewController.swift
@@ -11,18 +11,37 @@ import MapKit
 import CoreLocation
 import MessageUI
 
-class ViewController: UIViewController {
+// Latitude e longitude iniciais = -23.565811, -46.652478
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     // MARK: - Outlets
     @IBOutlet weak var map: MKMapView!
     @IBOutlet weak var latlongLabel: UILabel!
     
     // MARK: - Properties
+    
+    var gpsManager = CLLocationManager()
+    
 
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let initialCoordinates = CLLocationCoordinate2D(latitude: -23.565811, longitude: -46.652478)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+        
+        let initialRegion = MKCoordinateRegion(center: initialCoordinates, span: span)
+        
+        self.map.setRegion(initialRegion, animated: true)
+        
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedWhenInUse){
+            
+            self.gpsManager.requestWhenInUseAuthorization()
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,14 +54,38 @@ class ViewController: UIViewController {
     // MARK: - Actions
     @IBAction func trackPosition(_ sender: UIBarButtonItem) {
         
+        self.gpsManager.delegate = self
         
+        self.map.showsUserLocation = true
+        
+        self.gpsManager.startUpdatingLocation()
         
     }
+    
     @IBAction func sharePositionWithEmail(_ sender: UIBarButtonItem) {
         
         
         
     }
+    
+    // MARK: - CLLocationManagerDelegate Methods
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let location = locations.last {
+            
+            let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+            
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            
+            self.map.setRegion(region, animated: true)
+            
+        }
+        
+        
+
+        
+    }
+    
 }
 
