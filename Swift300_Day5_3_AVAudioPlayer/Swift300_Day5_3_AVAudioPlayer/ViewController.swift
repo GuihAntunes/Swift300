e36349617b4cbc71f522cb04fe8e32c9b838292b
Swift <swift@WozMac5.local>
2017-02-11 17:10:21 -0200

    implementing play button logic

--- Swift300_Day5_3_AVAudioPlayer/Swift300_Day5_3_AVAudioPlayer/ViewController.swift
+++ Swift300_Day5_3_AVAudioPlayer/Swift300_Day5_3_AVAudioPlayer/ViewController.swift
@@ -7,22 +7,85 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     // MARK: - Outlets
+    @IBOutlet weak var labelRate: UILabel!
     
     // MARK: - Properties
+    
+    var myPlayer : AVAudioPlayer!
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.setup()
+        
     }
     
     // MARK: - Methods
     
+    func setup() {
+        
+        self.labelRate.text = ""
+        
+    }
+    
     // MARK: - Actions
+    @IBAction func play(_ sender: UIButton) {
+        
+        // Criando o caminho para o resgate do arquivo
+        let filePath = Bundle.main.path(forResource: "dubstep", ofType: "m4a")
+        
+        let fileURL = URL(fileURLWithPath: filePath!)
+        
+        // Iniciando o nosso audio player
+        self.myPlayer = try? AVAudioPlayer(contentsOf: fileURL)
+        
+        // Propriedade que define o número de vezes que o audio irá ocorrer
+        self.myPlayer.numberOfLoops = 2
+        
+        // ==========
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        do{
+            
+            try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+            
+        }catch{}
+        
+        do{
+            
+            try audioSession.setActive(true)
+            
+        }catch{}
+        // ==========
+        
+        // Método que prepara o device
+        self.myPlayer.prepareToPlay()
+        
+        // Dando o play
+        self.myPlayer.play()
+        
+    }
+    
+    @IBAction func pause(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    @IBAction func changeRate(_ sender: UISlider) {
+        
+        
+        
+    }
+    
+
     
 }
 
