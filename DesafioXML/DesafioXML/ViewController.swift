181d313269a71a84452b64577ea969953f5af277
Swift <swift@WozMac5.local>
2017-02-11 13:45:18 -0200

    parsing XML

--- DesafioXML/DesafioXML/ViewController.swift
+++ DesafioXML/DesafioXML/ViewController.swift
@@ -8,19 +8,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, XMLParserDelegate {
     
     // MARK: - Outlets
     
     // MARK: - Properties
     
+    var array : [Dictionary<String,String>] = []
+    var studentDictionary = [String : String]()
+    var actualTag = ""
+    var tagContent = ""
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        if let filePath = Bundle.main.url(forResource: "dados", withExtension: "xml") {
+        
+            let xmlParser = XMLParser(contentsOf: filePath)
+            
+            xmlParser?.delegate = self
+            xmlParser?.parse()
+            
+        }
+        
+    }
+    
+    // MARK: - XMLParserDelegate Methods
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        
+        self.actualTag = elementName
+        
+        if elementName == "idade" || elementName == "nome" {
+            
+            self.tagContent = ""
+            
+        }
+        
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        
+        switch elementName {
+        case "nome":
+            
+            self.studentDictionary ["nome"] = self.tagContent
+            cleanVariables()
+            
+        case "idade":
+            
+            self.studentDictionary ["idade"] = self.tagContent
+            cleanVariables()
+           
+        case "aluno":
+            
+            self.array += [self.studentDictionary]
+        
+        default:
+            break
+        }
+        
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        
+        if self.actualTag == "nome" || self.actualTag == "idade" {
+            
+            self.tagContent += string
+            
+        }
+        
+    }
+    
+    func parserDidEndDocument(_ parser: XMLParser) {
+        
+        print(array)
+        
     }
     
     // MARK: - Methods
+    
+    func cleanVariables(){
+        
+        self.actualTag = ""
+        self.tagContent = ""
+        
+    }
     
     // MARK: - Actions
     
