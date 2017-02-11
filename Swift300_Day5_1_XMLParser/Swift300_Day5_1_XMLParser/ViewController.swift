7cd1aef646926e1edfff605d9e7fec0eec970dab
Swift <swift@WozMac5.local>
2017-02-11 09:25:37 -0200

    Creating project and marking up ViewController

---
+++ Swift300_Day5_1_XMLParser/Swift300_Day5_1_XMLParser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swift300_Day5_1_XMLParser
+//
+//  Created by Swift on 11/02/17.
+//  Copyright © 2017 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    // MARK: - Properties
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - Actions
+    
+}
